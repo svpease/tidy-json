7c7581c79b53cbb6b94f0c0aa4d294fb76efc8da
Steven Pease <svpease@Stevens-MBP.attlocal.net>
2020-12-25 04:54:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# tidy-json
+
+Simple recursive serializer of a JSON object into a string with newlines and spaces.
+
+# Install
+
+Install using npm:
+```console
+npm i tidy-json;
+```
+
+### Usage
+
+```js
+import tidyJson from 'tidy-json';
+console.log(tidyJson({'hi':'world',something:['4','a','13ois', true, false, null, '42', 34], somethingElse:{'hey':'WORLD!', s3: {'new': 'saopg!!!'}}}));
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const indentSpaces = 2;
+
+const spacesPrefix = numberOfSpaces => ' '.repeat(numberOfSpaces);
+
+export default (json, spaces = 0) => {
+  let result = '';
+
+  if (Array.isArray(json)) {
+    if (!json.length) return '[]\n';
+
+    result += '[\n';
+    json.forEach(arrayItem => {
+      result += spacesPrefix(spaces + indentSpaces) + print(arrayItem, spaces + indentSpaces);
+    });
+    result += spacesPrefix(spaces) + ']\n';
+  }
+  else if (typeof json === 'object') {
+    if (json === null) return 'null\n';
+
+    const keys = Object.keys(json);
+
+    if (!keys.length) return '{}\n';
+
+    result += '{\n';
+    keys.forEach(objectKey => {
+      result += spacesPrefix(spaces + indentSpaces) + `"${objectKey}": ` + print(json[objectKey], spaces + indentSpaces);
+    });
+    result += spacesPrefix(spaces) + '}\n';
+  }
+  else if (typeof json === 'string') {
+    result += `"${json}"` + '\n';
+  }
+  else {
+    result += json + '\n';
+  }
+
+  return result;
+};
